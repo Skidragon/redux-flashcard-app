5a63291eb628f522bdde11f09b954d1aa5fb4b48
Simon <Simon_Freelance@hotmail.com>
2018-07-14 20:55:27 -0400

    Imported the HomeContainer component and added it

--- src/app/App.js
+++ src/app/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import { Route } from 'react-router-dom';
+// import { Route } from 'react-router-dom';
+import HomeContainer from './home/HomeContainer';
 const AppContainer = styled.div`
 `;
 class App extends Component {
   render() {
     return (
       <AppContainer>
-        
+        <HomeContainer />
+
       </AppContainer>
     );
   }
